c04f264776e0dfdec6f39737af2f55e63332af45
MayankJain06 <mayank.jain@dhuni.tech>
2024-11-25 21:26:41 +0530

    Episode -01

---
+++ src/Components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+
+    return (
+        <div>
+            <Header />
+            <div>
+                <img src="https://assets.nflxext.com/ffe/siteui/vlv3/03ad76d1-e184-4d99-ae7d-708672fa1ac2/web/IN-hi-20241111-TRIFECTA-perspective_0cd6cd9f-ff1b-4eb6-a5ec-9f54293bf5b9_large.jpg" alt="logo" />
+            </div>
+        </div>
+    )
+}
+
+export default Login;
